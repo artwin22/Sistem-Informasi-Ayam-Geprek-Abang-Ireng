83fe1e874e8750917f213e460ef1557aee69c823
artwin22 <muhamad.14116050@student.itera.ac.id>
2019-04-29 11:08:05 +0700

    Update login_act.php

--- login_act.php
+++ login_act.php
@@ -1,9 +1,10 @@
 <?php 
+session_start();
 include 'admin/config.php';
 $uname=$_POST['uname'];
 $pass=$_POST['pass'];
 $pas=md5($pass);
-$aVar = mysqli_connect("localhost", "root", "","kios");
+$aVar = mysqli_connect("localhost", "root", "", "kios");
 $query=mysqli_query($aVar, "select * from admin where uname='$uname' and pass='$pas'")or die(mysqli_error($aVar));
 if(mysqli_num_rows($query)==1){
 	$_SESSION['uname']=$uname;
